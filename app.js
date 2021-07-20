7619f10832232fa59e9ef8abb9f45fb14f6008d8
Tiger Li <tiger01tgr@gmail.com>
2021-07-20 18:57:03 +0800

    added app.js, data.js, index.html, main.css, streamer_list.csv

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
+import {data} from "./data.js";
 
-const data = [];
-const fileInput = document.querySelector('.fileInput');
+const num = 3;
+
+var correctCount = 0;
+
+var winner;
+
+window.addEventListener("load", loadStreamer);
+
+
+
+function loadStreamer(){
+    document.getElementById("againBtn").style.display = "none";
+    document.getElementById("points").innerHTML = "Score: " + correctCount;
+
+    let s1 = Math.floor(Math.random() * num);
+    let s2 = Math.floor(Math.random() * num);
+    while(s2 == s1){
+        s2 = Math.floor(Math.random() * num);
+    }
+    if(s1 < s2){
+        winner = 1;
+    }
+    else{
+        winner = 2;
+    }
+
+
+    document.getElementById("pic1").src = data[s1][1];
+    document.getElementById("s1").innerHTML = data[s1][0];
+
+    document.getElementById("pic2").src = data[s2][1];
+    document.getElementById("s2").innerHTML = data[s2][0];
+
+}
+
+const pic1 = document.getElementById("pic1")
+const pic2 = document.getElementById("pic2")
+pic1.addEventListener("click", pic1Pick);
+pic2.addEventListener("click", pic2Pick);
+
+function pic1Pick(){
+    if(winner == 1){
+        won()
+    }
+    else{
+        lost()
+    }
+
+}
+
+function pic2Pick(){
+    if(winner == 2){
+        won()
+    }
+    else{
+        lost()
+    }
+
+;}
+
+function won(){
+    correctCount++;
+    loadStreamer();
+}
+
+function lost(){
+    document.getElementById("container").style.display = "none";
+    document.getElementById("points").innerHTML = "Final Score: " + correctCount;
+    document.getElementById("againBtn").style.display = "block";
+}
+
+
+const againBtn = document.getElementById("againBtn");
+againBtn.addEventListener("click", playAgain);
+
+function playAgain(){
+    document.getElementById("container").style.display = "block";
+    correctCount = 0;
+    loadStreamer();
+}
---
+++ data.js
@@ -0,0 +1,7 @@
+let data = [
+    ['/tommyinnit', 'https://static-cdn.jtvnw.net/jtv_user_pictures/1a50f189-3f73-4a93-a6e4-d53e8a26fceb-profile_image-300x300.jpeg'],
+    ['/tubbo', 'https://static-cdn.jtvnw.net/jtv_user_pictures/905af69a-4fd8-42c7-b842-bf4ee4d51b3b-profile_image-300x300.png'],
+    ['/ranboolive', 'https://static-cdn.jtvnw.net/jtv_user_pictures/8de4d844-586d-4be9-8570-04a0678fa99e-profile_image-300x300.png']
+];
+
+export {data};
