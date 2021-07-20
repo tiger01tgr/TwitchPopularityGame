eb6e8595d0719d025039d920d6a4ec6439f5950b
Tiger Li <tiger01tgr@gmail.com>
2021-07-20 20:02:26 +0800

    testing xmlhttp

--- data.js
+++ data.js
@@ -1,3 +1,30 @@
+
+const filePath = "streamer_list.csv"
+
+window.addEventListener("load", loadFile);
+
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onload = function(){
+        console.log(this.responseText);
+    };
+
+    xmlhttp.open("GET", filePath, true);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+}
+
+
+
+
+
+
 let data = [
     ['/tommyinnit', 'https://static-cdn.jtvnw.net/jtv_user_pictures/1a50f189-3f73-4a93-a6e4-d53e8a26fceb-profile_image-300x300.jpeg'],
     ['/tubbo', 'https://static-cdn.jtvnw.net/jtv_user_pictures/905af69a-4fd8-42c7-b842-bf4ee4d51b3b-profile_image-300x300.png'],
